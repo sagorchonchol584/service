34292e41cd1b7278cb2fec8eab46db97308389e3
CHONCHOL MAHAHMUD <77136983+sagorchonchol584@users.noreply.github.com>
2021-01-13 11:00:27 -0800

    me

--- app/src/main/java/com/example/service/MainActivity.java
+++ app/src/main/java/com/example/service/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView signup_button;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup_button=(TextView)findViewById(R.id.signup);
+
+
+
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  Intent i = new Intent(getApplicationContext(), MainView.class);
+            //    startActivity(i);
+                Toast.makeText(getApplicationContext(),"SIGN UP",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
